58c4885759b06e4c219f49ac826e3dc574b1b01f
wilderperozo <wilderperozo@gmail.com>
2022-09-05 00:32:32 +0200

    Initial commit - Login

---
+++ pages/auth/login.js
@@ -0,0 +1,112 @@
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import styles from "./login.module.css";
+
+export default function Login({ pokemon }) {
+  return (
+    <>
+      <style jsx>{`
+        .linear-right {
+          background: linear-gradient(
+            to right,
+            #ee7724,
+            #d8363a,
+            #dd3675,
+            #b44593
+          );
+        }
+      `}</style>
+      <section className="h-full gradient-form bg-gray-200 md:h-screen">
+        <div className="container py-12 px-6 h-full mx-auto">
+          <div className="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
+            <div className="xl:w-10/12">
+              <div className="block bg-white shadow-lg rounded-lg">
+                <div className="lg:flex lg:flex-wrap g-0">
+                  <div className="lg:w-6/12 px-4 md:px-0">
+                    <div className="md:p-12 md:mx-6">
+                      <div className="flex items-center justify-center">
+                        <Image
+                          src="/logo.png"
+                          alt="Vercel Logo"
+                          width={100}
+                          height={100}
+                        />{" "}
+                        <span className={styles.textLogo}>
+                          Pokemon Cards Battle
+                        </span>
+                      </div>
+                      <form>
+                        <p className="mb-4">Let's start!</p>
+                        <div className="mb-4">
+                          <input
+                            type="text"
+                            className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                            id="exampleFormControlInput1"
+                            placeholder="Username"
+                          />
+                        </div>
+                        <div className="mb-4">
+                          <input
+                            type="password"
+                            className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                            id="exampleFormControlInput1"
+                            placeholder="Password"
+                          />
+                        </div>
+                        <div className="text-center pt-1 mb-12 pb-1">
+                          <button
+                            className="bg-blue-500 inline-block px-6 py-2.5 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full mb-3"
+                            type="button"
+                            data-mdb-ripple="true"
+                            data-mdb-ripple-color="light"
+                          >
+                            Log in
+                          </button>
+                          <a className="text-gray-500" href="#!">
+                            Forgot password?
+                          </a>
+                        </div>
+                        <div className="flex items-center justify-between pb-6">
+                          <p className="mb-0 mr-2">Don't have an account?</p>
+                          <button
+                            type="button"
+                            className="inline-block px-6 py-2 border-2 border-red-600 text-red-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+                            data-mdb-ripple="true"
+                            data-mdb-ripple-color="light"
+                          >
+                            Sign up!
+                          </button>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                  <div className="lg:w-6/12 flex items-center lg:rounded-r-lg rounded-b-lg lg:rounded-bl-none linear-right justify-center">
+                  <div className={styles.backgroundPokemon}></div>
+                    <img
+                      className="flex justify-center z-10"
+                      src={pokemon}
+                      alt="Random Pokemon"
+                      width={300}
+                      height={300}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export async function getServerSideProps() {
+  const id = Math.floor(Math.random() * (150 - 1 + 1)); // get random number between 1 and 150
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+  const { sprites } = await response.json();
+  return {
+    props: {
+      pokemon: sprites.other.dream_world.front_default,
+    },
+  };
+}
